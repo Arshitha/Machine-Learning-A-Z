ced0d769e7a10e2ecbfa712e1c02d26dc58386d0
Arshitha <arsh2794@gmail.com>
2018-03-29 23:57:09 -0400

    today's progress

--- Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/data_preprocess_temp.py
+++ Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/data_preprocess_temp.py
@@ -16,7 +16,7 @@ dataset = pd.read_csv('Data.csv')
 X= dataset.iloc[:, :-1].values # matrix of features
 y = dataset.iloc[:,3].values # vector of labels
 
-# print(dataset)
+#print(dataset)
 
 # dealing with missing data
 # replace the missing data with mean of that data
@@ -27,6 +27,32 @@ imputer = imputer.fit(X[:,1:3]) # equality because the same imputer object was f
 X[:,1:3] = imputer.transform(X[:,1:3])
 
 # missing values replaced by their mean
+#print(X)
+
+# encode categorical data
+# in our dataset we have 2 categorical data categories. Country and purchased
+# since categories are strings and algorithms understand on;y numbers these data points
+# need to be encoded as numbers
+
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+# object of LabelEncoder class is labelencoder
+labelencoder_X = LabelEncoder()
+X[:,0]=labelencoder_X.fit_transform(X[:,0])
+# print(X)
+# at this point countries have been encoded with 0,1 and 2. However, even this 
+# is a problem because the algorithm could still interpret it as something
+# with relational order even though that's not the case. 
+
+# to deal with this unintended assignment of order, dummy variables are 
+# created
+
+# we'll create 3 separate columns, one for each country instead of the one comprehensive 
+onehotencoder = OneHotEncoder(categorical_features=[0])
+X = onehotencoder.fit_transform(X).toarray()
 print(X)
+
+
+
+
 
 
