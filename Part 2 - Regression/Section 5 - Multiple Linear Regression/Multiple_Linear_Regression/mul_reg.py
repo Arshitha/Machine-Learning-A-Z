bd9c0bd10c26ace79566b52acf0e85a308f50a03
Arshitha <arsh2794@gmail.com>
2018-04-04 23:54:21 -0400

    46th Day progress

--- Part 2 - Regression/Section 5 - Multiple Linear Regression/Multiple_Linear_Regression/mul_reg.py
+++ Part 2 - Regression/Section 5 - Multiple Linear Regression/Multiple_Linear_Regression/mul_reg.py
@@ -27,3 +27,28 @@
 # dummyVar1 = 1 - dummyVar2, this equation implies dummyVar1 is dependent on 
 # dummyVar2 and hence what happens is that the model cannot distinguish
 # the effects of dummyVar1 from dummyVar2. 
+
+
+# Building a model step by step	
+# Question 1: Which features to include in building a model and which 
+# ones to throw out? 
+# 1. Garbage in, garbage out
+# 2. Need to explain the correlations between the independent variables
+# and the dependent variables more than just mathematically.
+
+import pandas as pd
+import numpy as np 
+import matplotlib.pyplot as plt 
+
+dataset = pd.read_csv('50_Startups.csv')
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, 4]
+
+# encoding categorical variable
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+labelencoder_X = LabelEncoder()
+X[:,3] = labelencoder_X.fit_transform(X[:,3])
+onehotencoder = OneHotEncoder(categorical_features=[3])
+X = onehotencoder.fit_transform(X).toarray()
+#from sklearn.cross_validation import train_test_split
+print(X)
